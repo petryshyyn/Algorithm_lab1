c45b35e37c9e9cf0ef225b04643a1b037f689c8f
Ihor Petryshyn <44348379+petryshyyn@users.noreply.github.com>
2019-10-06 21:57:53 +0300

    Add lab2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 8, 4, 9};
+        int n = 3;
+        Algorithm algorithm = new Algorithm();
+        System.out.println(algorithm.search(array, n));
+    }
+}
