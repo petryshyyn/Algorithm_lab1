5cf172f856f8a40c907959034212b2b1b85a9a53
Ihor Petryshyn <44348379+petryshyyn@users.noreply.github.com>
2019-10-22 00:53:27 +0300

    Update Algorithm.java

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -3,28 +3,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Algorithm {
-    int search(int[] array, int n){
+    void search(int[] array, int cows) {
         List<Integer> list = new ArrayList<>();
-        List<Integer> otherList = new ArrayList<>();
-        List<Integer> resultList = new ArrayList<>();
-        for(int i : array){
+        for (int i : array) {
             list.add(i);
         }
         System.out.println(list + "- free places");
         list.sort(Integer::compareTo);
+        int maxStep = list.get(list.size() - 1) - list.get(0);
         System.out.println(list + "- sort list of places");
-        if(n == 2){
-            return list.get(list.size()-1)- list.get(0);
+        if (cows == 2) {
+            System.out.println("result: " + maxStep);
+        } else if (cows == list.size()) {
+            List<Integer> resultList = new ArrayList<>();
+            for (int i = 0; i < list.size() - 1; i++) {
+                resultList.add(list.get(i + 1) - list.get(i));
+            }
+            System.out.println("Result: " + Collections.min(resultList));
+        } else {
+            distribution(list, maxStep, cows);
         }
-        int step = (list.size()+1)/n;
-        for(int i =0; i <= list.size()-1;i+=step){
-            otherList.add(list.get(i));
+    }
+
+    private void distribution(List<Integer> list, int step, int cows) {
+        List<Integer> myList = new ArrayList<>();
+        myList.add(list.get(0));
+        for (int i = 1; i <= list.size() - 1; i++) {
+            if ((myList.get(myList.size() - 1) + step) < list.get(i)) {
+                myList.add(list.get(i));
+            }
         }
-        System.out.println(otherList + "- the outermost places");
-        for(int i = 0; i < otherList.size()-1; i++){
-            resultList.add(otherList.get(i+1)- otherList.get(i));
+        if (myList.size() == cows) {
+            System.out.println(myList + "- the outermost places");
+            List<Integer> resultList = new ArrayList<>();
+            for (int i = 0; i < myList.size() - 1; i++) {
+                resultList.add(myList.get(i + 1) - myList.get(i));
+            }
+            System.out.println(resultList + "- the distance between the outermost places");
+            System.out.println("Result: " + Collections.min(resultList));
+        } else {
+            distribution(list, step - 1, cows);
         }
-        System.out.println(resultList + "- the distance between the outermost places");
-        return Collections.min(resultList);
     }
 }
